a683c2f927dc0de46e55df4beb19dde2ada3e63b
sapatevaibhav <sapatevaibhav@duck.com>
2024-02-18 19:58:28 +0530

    made settings tab better

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,63 +1,139 @@
 import 'package:flutter/material.dart';
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:ai_mitra/utils.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void openSettingsPopup(
   BuildContext context,
   String apiKey,
   Function(String) initializeGenerativeModel,
 ) {
+  final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
   showDialog(
     context: context,
-    builder: (BuildContext context) {
+    builder: (
+      BuildContext context,
+    ) {
       return AlertDialog(
-        title: const Text('Settings'),
+        title: const Text(
+          'Settings',
+        ),
         content: SizedBox(
           height: 200,
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              ListTile(
-                title: const Row(
-                  children: [
-                    Icon(
-                      Icons.vpn_key,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      'Change API Key',
-                    ),
-                  ],
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    10,
+                  ),
+                  color: Colors.blue.withOpacity(
+                    0.2,
+                  ),
                 ),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  DialogUtils.showApiKeyDialog(
-                    context,
-                    apiKey,
-                    initializeGenerativeModel,
-                  );
-                },
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      10,
+                    ),
+                  ),
+                  leading: const Icon(
+                    Icons.vpn_key_outlined,
+                    color: Colors.blue,
+                  ),
+                  title: const Text(
+                    'Change API Key',
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    DialogUtils.showApiKeyDialog(
+                      context,
+                      apiKey,
+                      initializeGenerativeModel,
+                    );
+                  },
+                ),
               ),
-              ListTile(
-                title: const Row(
-                  children: [
-                    Icon(
-                      Icons.dark_mode,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      'Switch theme',
-                    ),
-                  ],
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    10,
+                  ),
+                  color: isDarkMode
+                      ? Colors.white.withOpacity(
+                          0.2,
+                        )
+                      : Colors.black.withOpacity(
+                          0.2,
+                        ),
                 ),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  _switchTheme(context);
-                },
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      10,
+                    ),
+                  ),
+                  leading: Icon(
+                    isDarkMode
+                        ? Icons.light_mode_outlined
+                        : Icons.dark_mode_outlined,
+                    color: isDarkMode ? Colors.white60 : Colors.black,
+                  ),
+                  title: Text(
+                    'Switch theme',
+                    style: TextStyle(
+                      color: isDarkMode ? Colors.white60 : Colors.black,
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    _switchTheme(context);
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    10,
+                  ),
+                  color: Colors.yellow.withOpacity(
+                    0.3,
+                  ),
+                ),
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      10,
+                    ),
+                  ),
+                  leading: const Icon(
+                    Icons.code_outlined,
+                  ),
+                  title: Text(
+                    'Source Code',
+                    style: TextStyle(
+                      color: isDarkMode ? Colors.white60 : Colors.black,
+                    ),
+                  ),
+                  onTap: () {
+                    launchUrl(
+                      Uri.parse(
+                        "https://github.com/sapatevaibhav/ai_mitra",
+                      ),
+                    );
+                  },
+                ),
               ),
             ],
           ),
@@ -68,9 +144,13 @@ void openSettingsPopup(
 }
 
 void _switchTheme(BuildContext context) {
-  final themeMode = AdaptiveTheme.of(context).mode;
+  final themeMode = AdaptiveTheme.of(
+    context,
+  ).mode;
   final newThemeMode = themeMode == AdaptiveThemeMode.light
       ? AdaptiveThemeMode.dark
       : AdaptiveThemeMode.light;
-  AdaptiveTheme.of(context).setThemeMode(newThemeMode);
+  AdaptiveTheme.of(context).setThemeMode(
+    newThemeMode,
+  );
 }
