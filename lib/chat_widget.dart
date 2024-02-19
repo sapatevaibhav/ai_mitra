61dd9513e971c3a8ddeaa7e8dac4e7209c736383
sapatevaibhav <sapatevaibhav@duck.com>
2024-02-20 00:22:06 +0530

    added option to send image

--- lib/chat_widget.dart
+++ lib/chat_widget.dart
@@ -1,6 +1,11 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:speech_to_text/speech_to_text.dart';
@@ -15,22 +20,23 @@ class ChatWidget extends StatefulWidget {
 }
 
 class _ChatWidgetState extends State<ChatWidget> {
-  late ScrollController _scrollController;
   GenerativeModel? model;
   late ChatSession chat;
+  late ScrollController _scrollController;
   final TextEditingController _textController = TextEditingController();
   final FocusNode _textFieldFocus = FocusNode();
   bool _loading = false;
   String? _apiKey;
   List<Message> messages = [];
   final SpeechToText _speech = SpeechToText();
+  final ImagePicker _picker = ImagePicker();
 
   @override
   void initState() {
     super.initState();
-    _scrollController = ScrollController();
     _getApiKey();
     _initSpeech();
+    _scrollController = ScrollController();
   }
 
   @override
@@ -40,126 +46,18 @@ class _ChatWidgetState extends State<ChatWidget> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    var textFieldDecoration = InputDecoration(
-      contentPadding: const EdgeInsets.all(15),
-      hintText: 'Enter a prompt...',
-      border: OutlineInputBorder(
-        borderRadius: const BorderRadius.all(Radius.circular(14)),
-        borderSide: BorderSide(color: Theme.of(context).colorScheme.secondary),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderRadius: const BorderRadius.all(Radius.circular(14)),
-        borderSide: BorderSide(color: Theme.of(context).colorScheme.secondary),
-      ),
-      suffixIcon: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          IconButton(
-            icon: const Icon(Icons.mic),
-            onPressed: () {
-              if (_speech.isAvailable) {
-                _speech.listen(
-                  onResult: (result) {
-                    if (result.finalResult) {
-                      setState(() {
-                        _textController.text = result.recognizedWords;
-                      });
-                      _sendChatMessage(result.recognizedWords);
-                    }
-                  },
-                  listenFor: const Duration(seconds: 5),
-                );
-              }
-            },
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.image,
-            ),
-          )
-        ],
-      ),
-    );
-
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: _apiKey != null && _apiKey!.isNotEmpty
-                ? SingleChildScrollView(
-                    controller: _scrollController,
-                    reverse: true,
-                    child: Column(
-                      children: messages.map((message) {
-                        return MessageWidget(
-                          message: message,
-                        );
-                      }).toList(),
-                    ),
-                  )
-                : const Center(
-                    child: Text(
-                      'No API key found. Please provide an API Key.',
-                    ),
-                  ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 15),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    autofocus: true,
-                    focusNode: _textFieldFocus,
-                    decoration: textFieldDecoration,
-                    controller: _textController,
-                    onSubmitted: (String value) {
-                      _sendChatMessage(value);
-                    },
-                  ),
-                ),
-                const SizedBox.square(
-                  dimension: 15,
-                ),
-                _loading
-                    ? const CircularProgressIndicator()
-                    : IconButton(
-                        onPressed: () {
-                          _sendChatMessage(_textController.text);
-                        },
-                        icon: Icon(
-                          Icons.send,
-                          color: Theme.of(context).colorScheme.primary,
-                        ),
-                      ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _scrollToBottom() {
-    if (_scrollController.hasClients &&
-        _scrollController.position.extentAfter == 0) {
-      _scrollController.animateTo(
-        _scrollController.position.maxScrollExtent,
-        duration: const Duration(
-          milliseconds: 300,
-        ),
-        curve: Curves.easeOut,
-      );
-    }
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      _scrollToBottom();
+    });
   }
 
   Future<void> _initSpeech() async {
-    await _speech.initialize(onStatus: (status) {}, onError: (error) {});
+    await _speech.initialize(
+      onStatus: (status) {},
+      onError: (error) {},
+    );
   }
 
   Future<void> _getApiKey() async {
@@ -207,6 +105,7 @@ class _ChatWidgetState extends State<ChatWidget> {
             .map((messageJson) => Message.fromJson(messageJson))
             .toList();
       });
+      _scrollToBottom();
     }
   }
 
@@ -216,6 +115,114 @@ class _ChatWidgetState extends State<ChatWidget> {
         messages.map((message) => message.toJson()).toList();
 
     await prefs.setStringList('messages', messagesJson);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var textFieldDecoration = InputDecoration(
+      contentPadding: const EdgeInsets.all(15),
+      hintText: 'Enter a prompt...',
+      border: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(Radius.circular(14)),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(Radius.circular(14)),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+      suffixIcon: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            icon: const Icon(Icons.photo_library),
+            onPressed: () {
+              _getImageFromSource();
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.mic),
+            onPressed: () {
+              if (_speech.isAvailable) {
+                _speech.listen(
+                  onResult: (result) {
+                    if (result.finalResult) {
+                      setState(() {
+                        _textController.text = result.recognizedWords;
+                      });
+                      _sendChatMessage(result.recognizedWords);
+                    }
+                  },
+                  listenFor: const Duration(seconds: 5),
+                );
+              }
+            },
+          ),
+        ],
+      ),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: _apiKey != null && _apiKey!.isNotEmpty
+                ? ListView.builder(
+                    controller: _scrollController,
+                    itemCount: messages.length,
+                    itemBuilder: (context, idx) {
+                      return MessageWidget(
+                        message: messages[idx],
+                      );
+                    },
+                  )
+                : const Center(
+                    child: Text(
+                      'No API key found. Please provide an API Key.',
+                    ),
+                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 15),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    autofocus: true,
+                    focusNode: _textFieldFocus,
+                    decoration: textFieldDecoration,
+                    controller: _textController,
+                    onSubmitted: (String value) {
+                      _sendChatMessage(value);
+                    },
+                  ),
+                ),
+                const SizedBox.square(
+                  dimension: 15,
+                ),
+                _loading
+                    ? const CircularProgressIndicator()
+                    : IconButton(
+                        onPressed: () {
+                          _sendChatMessage(_textController.text);
+                        },
+                        icon: Icon(
+                          Icons.send,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Future<void> _sendChatMessage(String message) async {
@@ -237,6 +244,8 @@ class _ChatWidgetState extends State<ChatWidget> {
           messages.add(Message(sender: Sender.Bot, text: text));
           _saveMessages();
         });
+
+        await Future.delayed(Duration(milliseconds: 100));
         _scrollToBottom();
       }
     } catch (e) {
@@ -269,5 +278,110 @@ class _ChatWidgetState extends State<ChatWidget> {
         );
       },
     );
+  }
+
+  void _scrollToBottom() {
+    if (_scrollController.hasClients &&
+        _scrollController.position.extentAfter == 0) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(
+          milliseconds: 300,
+        ),
+        curve: Curves.easeOut,
+      );
+    }
+  }
+
+  Future<void> _getImageFromSource() async {
+    setState(() {
+      _loading = true;
+    });
+
+    final XFile? pickedFile = await _picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 80,
+      maxWidth: 1920,
+      maxHeight: 1080,
+    );
+
+    if (pickedFile != null) {
+      final String imagePath = pickedFile.path;
+      final String imageType =
+          imagePath.substring(imagePath.lastIndexOf('.') + 1).toLowerCase();
+
+      if (['png', 'jpeg', 'webp', 'heic', 'heif'].contains(imageType)) {
+        final File imageFile = File(imagePath);
+        await sendImageToBot(imageFile);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Unsupported image type')),
+        );
+      }
+    }
+
+    setState(() {
+      _loading = false;
+    });
+  }
+
+  Future<void> sendImageToBot(File imageFile) async {
+    setState(() {
+      _loading = true;
+    });
+
+    try {
+      final imageBytes = await imageFile.readAsBytes();
+
+      const chunkSize = 1024 * 1024;
+
+      final chunks = <Uint8List>[];
+      for (var i = 0; i < imageBytes.length; i += chunkSize) {
+        final chunk = Uint8List.fromList(imageBytes.sublist(
+            i,
+            i + chunkSize < imageBytes.length
+                ? i + chunkSize
+                : imageBytes.length));
+        chunks.add(chunk);
+      }
+
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      final String? storedApiKey = prefs.getString('api_key');
+      if (storedApiKey == null) {
+        throw Exception('API key not found');
+      }
+
+      final model = GenerativeModel(
+        model: 'gemini-pro-vision',
+        apiKey: storedApiKey,
+      );
+
+      final prompt = TextPart("What's in this picture?");
+
+      final responses = <String>[];
+      for (final chunk in chunks) {
+        final imagePart = DataPart('image/jpeg', chunk);
+
+        final response = await model.generateContent([
+          Content.multi([prompt, imagePart])
+        ]);
+
+        responses.add(response.text ?? '');
+      }
+
+      final combinedResponse = responses.join(' ');
+
+      setState(() {
+        _loading = false;
+        messages.add(Message(sender: Sender.Bot, text: combinedResponse));
+        _saveMessages();
+        _scrollToBottom();
+      });
+    } catch (e) {
+      setState(() {
+        _loading = false;
+      });
+      _showError(e.toString());
+    }
   }
 }
